93ab79598a7f994d4dfc7f00c7ddbf8cc4b7a069
CornWorld <cw@had.ink>
2023-10-22 11:37:31 +0800

    [Feat] Support p10k instant prompt

--- scripts/init-zsh-kit.sh
+++ scripts/init-zsh-kit.sh
@@ -49,7 +49,7 @@ rm $HOME/install-ohmyzsh.sh
 # Install Powerlevel10k
 git clone --depth=1 $GITHUB_REPO_PREFIX/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# Confirm whether ~/.zshrc existed
+# Confirm whether $HOME/.zshrc existed
 if [[ -f "$HOME/.zshrc" ]]; then
     # Use grep and sed to set theme
     grep -q 'ZSH_THEME="powerlevel10k/powerlevel10k"' $HOME/.zshrc || sed -i 's|ZSH_THEME=".*"|ZSH_THEME="powerlevel10k/powerlevel10k"|' $HOME/.zshrc
@@ -58,21 +58,32 @@ else
     echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' > $HOME/.zshrc 
 fi
 
-echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> $HOME/.zshrc
-
 # Download .p10k.zsh and .dircolors to $HOME
 wget -O $HOME/.p10k.zsh $GITHUB_RAW_PREFIX/CornWorld/CornWorld/master/.p10k.zsh
 wget -O $HOME/.dircolors $GITHUB_RAW_PREFIX/CornWorld/CornWorld/master/.dircolors
 
+original_zshrc=$(cat $HOME/.zshrc)
+echo $original_zshrc > $HOME/.zshrc_backup_$RANDOM
 pt="
 alias rm='rm -I --preserve-root'
 alias chmod='chmod --preserve-root'
 alias chown='chown --preserve-root'
 alias chgrp='chgrp --preserve-root'
 "
+p10k_apply_config='
+[[ ! -f $HOME/.p10k.zsh ]] || source $HOME/.p10k.zsh
+'
+p10k_instant_prompt="
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+"
 
-echo $pt >> $HOME/.zshrc
 echo $pt >> $HOME/.bashrc
+echo $p10k_instant_prompt$original_zshrc$p10k_apply_config$pt > $HOME/.zshrc
 
 echo "======Enjoy!======"
 
