d3bbbdf2c2fec5f86d7a928a489164f3023b3264
CornWorld <cw@had.ink>
2023-10-22 14:27:18 +0800

    [Feat] Support more situations

--- scripts/init-zsh-kit.sh
+++ scripts/init-zsh-kit.sh
@@ -4,10 +4,9 @@
 
 # Description: Install zsh + oh-my-zsh + powerlevel10k on a initial archlinux where bash is already installed.
 
-GITHUB_REPO_PREFIX=https://github.com
-GITHUB_REPO_PREFIX_CN=https://hub.fgit.cf
-GITHUB_RAW_PREFIX=https://raw.githubusercontent.com
-GITHUB_RAW_PREFIX_CN=https://raw.fgit.cf
+GITHUB_PREFIX=https://github.com
+GITHUB_PREFIX_CN=https://ghproxy.com/https://github.com
+# GITHUB_PREFIX_CN=https://hub.fgit.cf
 
 echo "Please select your location:"
 echo "1. Chinese Mainland"
@@ -15,8 +14,7 @@ echo "2. Other"
 read -p "Enter your choice (1 or 2): " place
 case $place in
     1)
-        GITHUB_REPO_PREFIX=$GITHUB_REPO_PREFIX_CN
-        GITHUB_RAW_PREFIX=$GITHUB_RAW_PREFIX_CN
+        GITHUB_PREFIX=$GITHUB_PREFIX_CN
         ;;
     2)
         ;;
@@ -34,15 +32,23 @@ sudo pacman -Sy --noconfirm git wget zsh
 chsh -s /bin/zsh
 
 # Install ohmyzsh
-wget -O $HOME/install-ohmyzsh.sh $GITHUB_RAW_PREFIX/ohmyzsh/ohmyzsh/master/tools/install.sh
-sed -i "s|https://github.com|$GITHUB_REPO_PREFIX|" $HOME/install-ohmyzsh.sh
-sed -i 's|exec zsh -l||' $HOME/install-ohmyzsh.sh
-mv $HOME/.oh-my-zsh $HOME/.oh-my-zsh-backup_$RANDOM
-sh $HOME/install-ohmyzsh.sh
+wget -O $HOME/install-ohmyzsh.sh $GITHUB_PREFIX/ohmyzsh/ohmyzsh/raw/master/tools/install.sh
+sed -i "s|https://github.com|$GITHUB_PREFIX|" $HOME/install-ohmyzsh.sh
+if [[ -f "$HOME/.oh-my-zsh" ]]; then 
+    mv $HOME/.oh-my-zsh $HOME/.oh-my-zsh_backup_$RANDOM
+fi
+export RUNZSH=no
+sh $HOME/install-ohmyzsh.sh 
 rm $HOME/install-ohmyzsh.sh
 
+if [[ -f "$HOME/.oh-my-zsh/README.md" ]]; then 
+    rm $HOME/.oh-my-zsh
+    echo "[Error] ohmyzsh install failed: Network error. Cleaned up..."
+    exit -1
+fi
+
 # Install Powerlevel10k
-git clone --depth=1 $GITHUB_REPO_PREFIX/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone --depth=1 $GITHUB_PREFIX/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Confirm whether $HOME/.zshrc existed
 if [[ -f "$HOME/.zshrc" ]]; then
@@ -54,31 +60,34 @@ else
 fi
 
 # Download .p10k.zsh and .dircolors to $HOME
-wget -O $HOME/.p10k.zsh $GITHUB_RAW_PREFIX/CornWorld/CornWorld/master/.p10k.zsh
-wget -O $HOME/.dircolors $GITHUB_RAW_PREFIX/CornWorld/CornWorld/master/.dircolors
+wget -O $HOME/.p10k.zsh $GITHUB_PREFIX/CornWorld/CornWorld/raw/master/.p10k.zsh
+wget -O $HOME/.dircolors $GITHUB_PREFIX/CornWorld/CornWorld/raw/master/.dircolors
 
-original_zshrc=$(cat $HOME/.zshrc)
-echo $original_zshrc > $HOME/.zshrc_backup_$RANDOM
-pt="
+original_zshrc=$(cat "$HOME/.zshrc")
+echo "$original_zshrc" > "$HOME/.zshrc_backup_$RANDOM"
+
+pt=$(cat <<-'EOF'
 alias rm='rm -I --preserve-root'
 alias chmod='chmod --preserve-root'
 alias chown='chown --preserve-root'
 alias chgrp='chgrp --preserve-root'
-"
-p10k_apply_config='
+EOF
+)
+
+p10k_apply_config=$(cat <<-'EOF'
 [[ ! -f $HOME/.p10k.zsh ]] || source $HOME/.p10k.zsh
-'
-p10k_instant_prompt='
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
+EOF
+)
+
+p10k_instant_prompt=$(cat <<-'EOF'
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
-'
+EOF
+)
 
-echo $pt >> $HOME/.bashrc
-echo $p10k_instant_prompt$original_zshrc$p10k_apply_config$pt > $HOME/.zshrc
+echo -e "\n$pt\n" >> "$HOME/.bashrc"
+echo -e "$p10k_instant_prompt\n$p10k_apply_config\n\n$original_zshrc\n\n$pt\n" > "$HOME/.zshrc"
 
 echo "======Enjoy!======"
 
